5b9c2f4b6b3d93edbebc35097b928f10878b8dc5
Jason Hamilton-Smith <hs.jason@gmail.com>
2017-09-07 20:55:23 -0700

    Basic asteroids-style movement

--- src/main.lua
+++ src/main.lua
@@ -1,14 +1,49 @@
 
 function love.load()
-    r_x = 100
-    r_y = 50
+    p_x = 100
+    p_y = 50
+    v_x = 0
+    v_y = 0
+    rot = 0
+
+    accel = 100
+    rot_spd = 4
 end
 
 function love.update(dt)
-    r_x = r_x + 5 * dt
-    r_y = r_y + 5 * dt
+    p_x = p_x + v_x * dt
+    p_y = p_y + v_y * dt
+
+    if love.keyboard.isDown('w') then
+        comp_x = -math.sin(rot)
+        comp_y = math.cos(rot)
+        v_y = v_y - accel * dt * comp_y
+        v_x = v_x - accel * dt * comp_x
+    end
+    if love.keyboard.isDown('a') then
+        rot = rot - rot_spd * dt
+    end
+    if love.keyboard.isDown('d') then
+        rot = rot + rot_spd * dt
+    end
+
 end
 
 function love.draw()
-    love.graphics.rectangle('line', r_x, r_y, 200, 150)
+    draw_ship(p_x, p_y, rot)
+end
+
+
+function draw_ship(x, y, r)
+    love.graphics.push()
+    love.graphics.translate(x, y)
+    love.graphics.rotate(r)
+
+    love.graphics.line(
+          0, -15,
+        -10,  15,
+         10,  15,
+          0,   -15)
+
+    love.graphics.pop()
 end
