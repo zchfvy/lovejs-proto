2d2d461b13b459752919bc51184de1ae690804bd
Jason Hamilton-Smith <hs.jason@gmail.com>
2017-09-09 20:00:59 -0700

    Add basic bullets

--- src/main.lua
+++ src/main.lua
@@ -12,24 +12,39 @@ function love.load()
     accel = 100
     rot_spd = 4
 
+    bullets = {}
+    shooting = 0
+    bul_spd = 150
+
     love.window.setMode(screen_x, screen_y, {})
 end
 
 function love.update(dt)
     p_x = p_x + v_x * dt
     p_y = p_y + v_y * dt
+    comp_x = math.sin(rot)
+    comp_y = -math.cos(rot)
 
     if love.keyboard.isDown('w') then
-        comp_x = -math.sin(rot)
-        comp_y = math.cos(rot)
-        v_y = v_y - accel * dt * comp_y
-        v_x = v_x - accel * dt * comp_x
+        v_y = v_y + accel * dt * comp_y
+        v_x = v_x + accel * dt * comp_x
     end
     if love.keyboard.isDown('a') then
         rot = rot - rot_spd * dt
     end
     if love.keyboard.isDown('d') then
         rot = rot + rot_spd * dt
+    end
+    if love.keyboard.isDown('space') and shooting == 0 then
+        shooting = 1
+        table.insert(bullets, 
+            {x = p_x, y = p_y,
+             vx = v_x + bul_spd*comp_x,
+             vy = v_y + bul_spd*comp_y}
+            )
+    end
+    if not love.keyboard.isDown('space') then
+        shooting = 0
     end
 
     if p_x > screen_x then
@@ -44,10 +59,18 @@ function love.update(dt)
     if p_y < 0 then
         p_y = p_y + screen_y
     end
+
+    for id, bullet in pairs(bullets) do
+        bullet.x = bullet.x + bullet.vx * dt
+        bullet.y = bullet.y + bullet.vy * dt
+    end
 end
 
 function love.draw()
     draw_ship(p_x, p_y, rot)
+    for id, bullet in pairs(bullets) do
+        love.graphics.points(bullet.x, bullet.y)
+    end
 end
 
 
